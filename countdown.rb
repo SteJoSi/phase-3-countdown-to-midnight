fdbe7059b615f121706b6b23e788ab97fda23d9b
Stephanie Sison <sjsison27@icloud.com>
2022-11-20 21:21:03 -0800

    commit tree 4ca6d8744bd842d078e1728aae653cd0e0f6c642

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+require 'pry'
 
-def countdown
+# takes in an integer, while loop counts down to 1
+# returns "HAPPY NEW YEAR!"
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+        sleep(5)
+    end
+    "HAPPY NEW YEAR!"
+end
+
+# binding.pry
